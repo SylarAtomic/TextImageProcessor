055bc251966d0cd467566e6df224544f91ed51a7
Marc Le Masson <sylaratomic@gmail.com>
2019-02-15 14:35:32 +0200

    Added notes to app.js for easier reading.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function processImage(){
 var ImageAPIUrl = "https://uksouth.api.cognitive.microsoft.com/vision/v2.0/ocr"; // Microsoft API Call page
-var ImageAPIKey = "1c20f181438e4b11a722d8eb2febf1b4"; // Please enter an API Key to use this feature
+var ImageAPIKey = "1c20f181438e4b11a722d8eb2febf1b4"; // Please enter an API Key to use this feature - My key will be regenerated when fill goes public.
     
     // Default parameters. Used in almost all cases
     var parameters = {
@@ -17,12 +17,12 @@ var ImageAPIKey = "1c20f181438e4b11a722d8eb2febf1b4"; // Please enter an API Key
     var info= "";
     
     // Get user image
-    var imageURL = document.getElementById("inputImage").value;
+    var imageURL = document.getElementById("inputImage").value;             // Get use image URL from HTML doc
     document.querySelector("#sourceImage").src = imageURL;
     
     // API Call - Time for some jQuery
     $.ajax({
-            url: ImageAPIUrl + "?" + $.param(parameters),
+            url: ImageAPIUrl + "?" + $.param(parameters),                   // ajax request start
         
         // Header
         beforeSend: function(jqXHR){
@@ -30,59 +30,34 @@ var ImageAPIKey = "1c20f181438e4b11a722d8eb2febf1b4"; // Please enter an API Key
           jqXHR.setRequestHeader("Ocp-Apim-Subscription-Key", ImageAPIKey)  // API access key
         },
         
-        type: "POST",
+        type: "POST",                                                       // Request type
         
         // Body
         data: '{"url": ' + '"' + imageURL + '"}',
         
-        success: function(data) {
-            console.log('Standard', data);
-            console.log(JSON.stringify(data, null, 2));
+        success: function(data) {                                           
+            //console.log('Standard', data);                                 // Used for debugging
+            //console.log(JSON.stringify(data, null, 2));                    // Used for debugging     
             var obj = JSON.parse(JSON.stringify(data, null, 2));
-
-            
-            /*$.each(data, function(i, info){
-                info += (obj.regions[0].lines[0].words[i].text);
-            })*/
-            //console.log(obj);
-            
-            //console.log(obj.regions.lines.words.text);
-            
-            //var numWords = obj.regions[0].lines[j].words.length;
             
             var numLines = obj.regions[0].lines.length;
             
-            for(var j = 0; j < numLines; j++)
+            for(var j = 0; j < numLines; j++)                               // Loop to extract only words that are returned
                 {
-                    var numWords = obj.regions[0].lines[j].words.length;
+                    var numWords = obj.regions[0].lines[j].words.length;    
                         for(var i = 0; i < numWords; i++){
                             info += obj.regions[0].lines[j].words[i].text + ' ';
-                                /*if(i = obj.regions[0].lines[j].words[i].lastIndex){
-                                   info += 'last of';
-                                   }*/
                             }
                             info += '\n';
                 }
-           // console.log(info);
-
         }
         
     })
     
     // Successful
     .done(function(data) {
-        // Display image text
-       // $("#responseTextArea").val(JSON.stringify(data, null, 2));
-        /*$("#responseTextArea").val(JSON.stringify(data, function(key, value){
-            if (key == "text") {
-                return value;
-            }
-        }, 2));*/
         
-        //var jsonObj = JSON.parse(data);
-        
-        // $("#responseTextArea").val(jsonObj.text); Changes this back to what it should be!
-        $("#responseTextArea").val(info);
+        $("#responseTextArea").val(info);                                   // Sending words found to responce box
         
         
     })
@@ -98,20 +73,4 @@ var ImageAPIKey = "1c20f181438e4b11a722d8eb2febf1b4"; // Please enter an API Key
             alert(errorString);
     });
 };
-
-
-
-
-
-
-// curl "https://<region>.api.cognitive.microsoft.com/vision/v2.0/ocr" \
-// -H "Ocp-Apim-Subscription-Key: $key" \
-// -H "Content-Type: application/json"  \
-// -d "{'url' : 'https://raw.githubusercontent.com/MicrosoftDocs/mslearn-process-images-with-the-computer-vision-service/master/images/ebook.png'}" \
-// | jq '.'
-
-
-// https://static.makeuseof.com/wp-content/uploads/2018/04/Save-Water.jpg
-
-// https://raw.githubusercontent.com/MicrosoftDocs/mslearn-process-images-with-the-computer-vision-service/master/images/ebook.png
 
